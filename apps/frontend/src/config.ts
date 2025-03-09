3134e7a2372570fb3f2ca64880fe8ca1a5ac6658
SKYV <dennisdekkers16@gmail.com>
2025-03-09 15:19:46 +0100

    up

--- apps/frontend/src/config.ts
+++ apps/frontend/src/config.ts
@@ -1,1 +1,1 @@
-export const backendURL = "http://localhost:3000"; // TODO change to your backend URL
+export const backendURL = "http://localhost:3000"; // TODO change to your backend URL"http://191.96.53.14:3000"
